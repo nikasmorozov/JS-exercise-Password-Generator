647878a316dbbc98c3bac0ae83ac64c059084a4b
Nikas <nikas@Nikas-MBP.lan>
2019-12-01 23:02:20 +0200

    function does work, styling needed

---
+++ script.js
@@ -0,0 +1,35 @@
+let serviceName = document.getElementById('serviceName');
+let servicePass = document.getElementById('servicePass');
+let vowels = 0;
+let nonVowels = 0;
+
+document.getElementById('generatePass').addEventListener('click', generatePass);
+
+function generatePass () {
+    let nameArray = serviceName.value.split('');
+    let passArray = servicePass.value.split('');
+    let combinedPass = [];
+
+    combinedPass.splice(0, 0, nameArray[nameArray.length - 1]);
+
+    nameArray.forEach(function (char) {
+        if (/^[aeiou]$/.test(char.toLowerCase())) {
+            vowels += 1
+        };
+    });
+    combinedPass.push(nameArray[vowels - 1]);
+
+    combinedPass.splice(2, 0, passArray.join(''));
+
+//    cia noriu padaryti single line/arrow
+    nameArray.forEach(function (char) {
+        if (!(/^[aeiou]$/.test(char.toLowerCase()))) {
+            nonVowels += 1
+        };
+    });
+    combinedPass.push(nonVowels);
+
+    combinedPass.push(nameArray[0]);
+
+    document.getElementById('output').textContent = combinedPass.join('');
+};
